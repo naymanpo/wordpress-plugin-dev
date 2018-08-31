b9c631cd0755561b10c0552aa435cf2f69c8c15d
nay man <nayman.po@gmail.com>
2018-08-31 20:07:02 +0700

    remove application user roles

--- wp-content/plugins/wpa-user-management/wpa-user-management.php
+++ wp-content/plugins/wpa-user-management/wpa-user-management.php
@@ -11,11 +11,12 @@
 
  class WPA_User_Manager{
      public function __construct(){
-        register_activation_hook(__FILE__,array($this,'add_application_user_rules'));   
-     }
+        register_activation_hook(__FILE__,array($this,'add_application_user_roles'));   
+        register_activation_hook(__FILE__,array($this,'remove_application_user_roles'));
+    }
 
 
-     public function add_application_user_rules(){
+     public function add_application_user_roles(){
         /*
         add rule
         $result = add_role( 'role_name', 'Display Name', array(
@@ -24,11 +25,22 @@
             'delete_posts' => false,
             ) );
         */
-         //add rule follower to wordpress
+         //add new user roles
          add_role('follower','Follwer',array('read',true));
          add_role('developer','Developer',array('read',true));
          add_role('member','Member',array('member',true));
      }
+
+
+     /**
+      * remove application user roles
+      */
+     public function remove_application_user_roles(){
+        remove_role('author');
+        remove_role('editor');
+        remove_role('contributor');
+        remove_role('subscriber');
+    }
  }
 
  $wpa_user_manager=new WPA_User_Manager();
